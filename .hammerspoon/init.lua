ee58a286daf922377f896ee25fd7f609150ba8ad
trankien386 <kien_iu_ai@yahoo.com>
2021-07-23 21:41:08 +0700

    change shortcut for control center

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,7 +4,7 @@ stackline:init()
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-hs.hotkey.bind({"ctrl"}, "c", function()
+hs.hotkey.bind({"ctrl"}, "x", function()
   hs.osascript.applescript([[
     tell application "System Events"
       tell process "Control Center"
